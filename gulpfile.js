5996af32694d3c6d661186286443dd82347cc905
Joel Gamble <jjoelgamble@gmail.com>
2021-06-07 22:46:47 -0500

    first commit to make this my main site. also adding powergrid files

--- gulpfile.js
+++ gulpfile.js
@@ -1,5 +1,7 @@
 const gulp = require("gulp");
 const sass = require("gulp-sass");
+const uglify = require('gulp-uglify');
+const imagemin = require('gulp-imagemin');
 
 /*
   generate the css with sass
@@ -16,6 +18,7 @@ gulp.task('css', function() {
 
 gulp.task('js', function() {
   return gulp.src("./src/js/**/*.js")
+    .pipe(uglify())
     .pipe(gulp.dest('_site/js'));
 });
 
@@ -35,3 +38,47 @@ gulp.task("watch", function() {
 gulp.task('build', gulp.parallel(
   'js'
 ));
+
+
+// Levelup Tuts tasks
+
+function errorLog(error) {
+  console.error.bind(error);
+  this.emit('end');
+}
+
+// Scripts Task
+// Uglifies
+gulp.task('scripts',function(){
+  gulp.src('./src/js/**/*.js')
+  .pipe(uglify())
+  .on('error', errorLog)
+  .pipe(gulp.dest('_site/js'));
+});
+
+//Image min Task
+gulp.task('image',function(){
+  gulp.src('./img/*')
+  .pipe(imagemin())
+  .pipe(gulp.dest('_site/img'));
+});
+
+
+// Styles Task
+gulp.task('styles', function() {
+  gulp.src('./src/scss/main.scss')
+  .pipe(sass({
+    outputStyle: 'compressed'
+  }))
+  .on('error', errorLog)
+  .pipe(gulp.dest('_site/css'));
+});
+
+// Watch JS Task
+gulp.task('watch', function() {
+  gulp.watch('./src/js/**/*.js', gulp.parallel('scripts'));
+  gulp.watch('./src/scss/main.scss', gulp.parallel('styles'));
+  gulp.watch('./img/*', gulp.parallel('image'));
+});
+
+gulp.task('default', gulp.parallel('scripts','styles','image','watch'));
